60f1d786345be5726b09d49c8769b0c7b47eee9f
Ali Gharachorloo <ali.gharachorloo@gmail.com>
2023-10-31 14:20:05 +0330

    add logs

--- datalayer-push.js
+++ datalayer-push.js
@@ -3,9 +3,11 @@ jQuery(document).ready(function() {
     var elementToLoad = jQuery('#mc_signup'); // Replace 'yourElement' with the ID or selector of the element you want to target
 
     elementToLoad.on('load', function() {
+        console.log('mc_signup is loaded');
         jQuery('#mc_signup_form').submit(function() {
             // Prevent the form from submitting
             event.preventDefault();
+            console.log('is submitted');
             console.log(jQuery('#mc_mv_EMAIL'));
             window.dataLayer = window.dataLayer || [];
             window.dataLayer.push({
