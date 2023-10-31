68407b3d48709ab538af6b6c8a4741fbd3b1db17
Ali Gharachorloo <ali.gharachorloo@gmail.com>
2023-10-31 14:29:38 +0330

    remove the prev code

--- datalayer-push.js
+++ datalayer-push.js
@@ -1,17 +1,14 @@
-
-jQuery(document).ready(function() {
-    var elementToLoad = jQuery('#mc_signup'); // Replace 'yourElement' with the ID or selector of the element you want to target
-
-    elementToLoad.on('load', function() {
-            // Prevent the form from submitting
-            event.preventDefault();
-            console.log('is submitted');
-            console.log(jQuery('#mc_mv_EMAIL'));
-            window.dataLayer = window.dataLayer || [];
-            window.dataLayer.push({
-                'event': 'signup_completed',
-                'email': jQuery('#mc_mv_EMAIL'),
-            });
-
+jQuery(document).ready(function($) {
+    $('#mc_signup_form').submit(function() {
+        // Prevent the form from submitting
+        event.preventDefault();
+        // Log the message to the console
+        console.log('form_submitted');
+        window.dataLayer = window.dataLayer || [];
+        window.dataLayer.push({
+            'event': 'signup_completed',
+            'email': 'test@test.com',
         });
+    });
 });
+
