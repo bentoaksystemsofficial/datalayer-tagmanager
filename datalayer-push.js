aa363e818b5e06f640409cdffe7d4314986a4e68
Ali Gharachorloo <ali.gharachorloo@gmail.com>
2023-10-31 10:03:37 +0330

    initate newsletter signup

--- datalayer-push.js
+++ datalayer-push.js
@@ -1,22 +1,18 @@
-jQuery(document).ready(function($) {
-    $('#mc_signup_form').submit(function() {
-        // Prevent the form from submitting
-        event.preventDefault();
-        // Log the message to the console
-        console.log('form_submitted');
-        // You can add additional logic here, e.g., send data to the server via AJAX
-    });
-});
-$(document).ready(function() {
-    var elementToLoad = $('#mc_signup'); // Replace 'yourElement' with the ID or selector of the element you want to target
+
+jQuery(document).ready(function() {
+    var elementToLoad = jQuery('#mc_signup'); // Replace 'yourElement' with the ID or selector of the element you want to target
 
     elementToLoad.on('load', function() {
-        $('#mc_signup_form').submit(function() {
+        jQuery('#mc_signup_form').submit(function() {
             // Prevent the form from submitting
             event.preventDefault();
-            // Log the message to the console
-            console.log('form_submitted');
-            // You can add additional logic here, e.g., send data to the server via AJAX
+            console.log(jQuery('#mc_mv_EMAIL'));
+            window.dataLayer = window.dataLayer || [];
+            window.dataLayer.push({
+                'event': 'signup_completed',
+                'email': jQuery('#mc_mv_EMAIL'),
+            });
+
         });
     });
 });
