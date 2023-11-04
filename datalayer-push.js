ac6629a33a4ebb62d71104fdc9b02b37f91cab76
Ali Gharachorloo <ali.gharachorloo@gmail.com>
2023-11-04 18:16:40 +0330

    update the form1

--- datalayer-push.js
+++ datalayer-push.js
@@ -3,11 +3,11 @@ jQuery(document).ready(function($) {
         // Prevent the form from submitting
         event.preventDefault();
         // Log the message to the console
-        console.log(jQuery('#mc_mv_EMAIL').text());
+        console.log(jQuery("#mc_mv_EMAIL").val());
         window.dataLayer = window.dataLayer || [];
         window.dataLayer.push({
             'event': 'signup_completed',
-            'email': jQuery('#mc_mv_EMAIL').text(),
+            'email': jQuery("#mc_mv_EMAIL").val(),
         });
     });
 });
